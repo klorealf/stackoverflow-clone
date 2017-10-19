1ef65341383d345e9179782e1e17f9d878aef82c
Amber Maillard <amber.maillard23@gmail.com>
2017-10-19 16:19:03 -0400

    Rework user controller and add routes to a sessions controller

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -8,32 +8,10 @@ post '/users' do
   @user = User.new(params[:user])
   if @user.save
     session[:user_id] = @user.id
-    redirect "/users/login"
+    redirect "/users/#{@user.id}"
   else
     erb :"users/new"
   end
-end
-
-#login
-get '/login' do
-  erb :"/users/login"
-end
-
-post '/login' do
-  @user = User.authenticate(params[:email], params[:password])
-  if @user
-    session[:user_id] = @user.id
-    redirect "/users/#{@user.id}"
-  else
-    @error = "Invalid login, try again"
-    erb :'users/login'
-  end
-end
-
-#logout
-get '/users/logout' do
-  session.clear
-  redirect "/"
 end
 
 #last item ALWAYS
