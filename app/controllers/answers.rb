b58307c4b894fafd07e84c7eab0a655c7d82222a
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-10-19 16:21:45 -0400

    And answer routes with no content

---
+++ app/controllers/answers.rb
@@ -0,0 +1,19 @@
+get "/answers" do
+  "Answers Home Page"
+end
+
+get "/answers/new" do
+  "New answer"
+end
+
+get "/answers/:id" do
+  "Show Answer"
+end
+
+post "/answers" do
+  "Post New Answer"
+end
+
+delete "/answers" do
+  "delete answer"
+end
